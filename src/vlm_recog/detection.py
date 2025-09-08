ddb6dc5ecb4b2a20fc70924cc919bc091f84b4a2
neka-nat <nekanat.stock@gmail.com>
2025-09-08 17:55:31 +0900

    bug fix

--- src/vlm_recog/detection.py
+++ src/vlm_recog/detection.py
@@ -61,15 +61,14 @@ def detect(
     items = json.loads(parse_json(response.text))
 
     detected_items: DetectedItems = []
-    factor = 1024 / 1000
     # Process each mask
     for item in items:
         # Get bounding box coordinates
         box = item["box_2d"]
-        y0 = int(box[0] / input_image_size[1] * image.size[1] * factor)
-        x0 = int(box[1] / input_image_size[0] * image.size[0] * factor)
-        y1 = int(box[2] / input_image_size[1] * image.size[1] * factor)
-        x1 = int(box[3] / input_image_size[0] * image.size[0] * factor)
+        y0 = int(box[0] / 1000 * image.size[1])
+        x0 = int(box[1] / 1000 * image.size[0])
+        y1 = int(box[2] / 1000 * image.size[1])
+        x1 = int(box[3] / 1000 * image.size[0])
 
         # Skip invalid boxes
         if y0 >= y1 or x0 >= x1:
